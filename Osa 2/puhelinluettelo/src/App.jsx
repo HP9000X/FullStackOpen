cc922fc296719e2a4cf9cfad395cd3e2f7e925f5
Arttu Savolainen <arttu.a.savolainen@tuni.fi>
2025-06-06 14:04:56 +0300

    Task 2.11 done
    Added fetching initial data from a json server

--- Osa 2/puhelinluettelo/src/App.jsx
+++ Osa 2/puhelinluettelo/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Filter = ({ newFilter, handleFilterInput }) => {
   return (
@@ -35,15 +36,16 @@ const Persons = ({filteredPersons}) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setFilter] = useState('')
+
+  useEffect(() => {
+  axios
+    .get("http://localhost:3001/persons")
+    .then((response => setPersons(response.data)))
+  }, [])
 
   const handleNameInput = (event) => {
     setNewName(event.target.value)
@@ -72,6 +74,7 @@ const App = () => {
   const filteredPersons = persons.filter(
     person => person.name.toLowerCase().includes(newFilter.toLowerCase())
   )
+  
 
 
   return (
